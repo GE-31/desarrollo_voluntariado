30f970b3242a420237f161e5438e21789cf138c5
JeslynHidrogo <jes.hidrogo26@gmail.com>
2026-02-28 00:37:24 -0500

    correccion

--- src/main/java/com/sistemadevoluntariado/entity/Beneficiario.java
+++ src/main/java/com/sistemadevoluntariado/entity/Beneficiario.java
@@ -51,8 +51,7 @@ public class Beneficiario {
     private LocalDateTime creadoEn;
 
     // Constructores
-    public Beneficiario() {
-    }
+    public Beneficiario() {}
 
     public Beneficiario(String nombres, String apellidos, String dni, String telefono,
             String direccion, String distrito, String tipoBeneficiario,
@@ -69,115 +68,32 @@ public class Beneficiario {
     }
 
     // Getters y Setters
-    public int getIdBeneficiario() {
-        return idBeneficiario;
-    }
-
-    public void setIdBeneficiario(int idBeneficiario) {
-        this.idBeneficiario = idBeneficiario;
-    }
-
-    public String getNombres() {
-        return nombres;
-    }
-
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
-    public LocalDate getFechaNacimiento() {
-        return fechaNacimiento;
-    }
-
-    public void setFechaNacimiento(LocalDate fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getDistrito() {
-        return distrito;
-    }
-
-    public void setDistrito(String distrito) {
-        this.distrito = distrito;
-    }
-
-    public String getTipoBeneficiario() {
-        return tipoBeneficiario;
-    }
-
-    public void setTipoBeneficiario(String tipoBeneficiario) {
-        this.tipoBeneficiario = tipoBeneficiario;
-    }
-
-    public String getNecesidadPrincipal() {
-        return necesidadPrincipal;
-    }
-
-    public void setNecesidadPrincipal(String necesidadPrincipal) {
-        this.necesidadPrincipal = necesidadPrincipal;
-    }
-
-    public String getObservaciones() {
-        return observaciones;
-    }
-
-    public void setObservaciones(String observaciones) {
-        this.observaciones = observaciones;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public Integer getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(Integer idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
-    public LocalDateTime getCreadoEn() {
-        return creadoEn;
-    }
-
-    public void setCreadoEn(LocalDateTime creadoEn) {
-        this.creadoEn = creadoEn;
-    }
+    public int getIdBeneficiario() { return idBeneficiario; }
+    public void setIdBeneficiario(int idBeneficiario) { this.idBeneficiario = idBeneficiario; }
+    public String getNombres() { return nombres; }
+    public void setNombres(String nombres) { this.nombres = nombres; }
+    public String getApellidos() { return apellidos; }
+    public void setApellidos(String apellidos) { this.apellidos = apellidos; }
+    public String getDni() { return dni; }
+    public void setDni(String dni) { this.dni = dni; }
+    public LocalDate getFechaNacimiento() { return fechaNacimiento; }
+    public void setFechaNacimiento(LocalDate fechaNacimiento) { this.fechaNacimiento = fechaNacimiento; }
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+    public String getDireccion() { return direccion; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+    public String getDistrito() { return distrito; }
+    public void setDistrito(String distrito) { this.distrito = distrito; }
+    public String getTipoBeneficiario() { return tipoBeneficiario; }
+    public void setTipoBeneficiario(String tipoBeneficiario) { this.tipoBeneficiario = tipoBeneficiario; }
+    public String getNecesidadPrincipal() { return necesidadPrincipal; }
+    public void setNecesidadPrincipal(String necesidadPrincipal) { this.necesidadPrincipal = necesidadPrincipal; }
+    public String getObservaciones() { return observaciones; }
+    public void setObservaciones(String observaciones) { this.observaciones = observaciones; }
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
+    public Integer getIdUsuario() { return idUsuario; }
+    public void setIdUsuario(Integer idUsuario) { this.idUsuario = idUsuario; }
+    public LocalDateTime getCreadoEn() { return creadoEn; }
+    public void setCreadoEn(LocalDateTime creadoEn) { this.creadoEn = creadoEn; }
 }
